b48da34d6852cc197c31774cae131c1b69a19916
Aditi Vinod <avinod@olin.edu>
2024-04-30 04:54:09 -0400

    Add structue for pages

--- personality-site/src/App.js
+++ personality-site/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Result from './pages/ResultPage/Result';
+import StartScreen from './pages/StartScreen/StartScreen';
+import TestQuestion from './pages/TestQuestion/TestQuestion';
 
 function App() {
   return (
