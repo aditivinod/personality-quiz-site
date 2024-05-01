72c5229aab94c01bb26ff7dbb7b64a586a6a72f8
Aditi Vinod <avinod@olin.edu>
2024-05-01 00:57:13 -0400

    Update image paths for TestQuestion

--- personality-site/src/pages/TestQuestion/TestQuestion.js
+++ personality-site/src/pages/TestQuestion/TestQuestion.js
@@ -5,7 +5,7 @@ function TestQuestion({questions, current}){
     console.log("Question Page.")
     return(
         <div className="page">
-            <img src="images/background.jpg" alt="Intro Page" className="page" style={{ height: '760px' }} />
+            <img src={process.env.PUBLIC_URL + "/assets/background.jpg"} alt="Intro Page" className="page" style={{ height: '760px' }} />
             <div className="container">
                 <div className="titleText">
                     <h1>Question 1</h1>
@@ -13,10 +13,11 @@ function TestQuestion({questions, current}){
                         <h4>The semester is winding to a close and you’re talking with your advisor to prepare for one of the most dreaded times of the year for college students: course selection.</h4>
                     </div>
                 </div>
-                <a href="assets/heart.jpg"><button type="button" className="answer1">"It’s alright- I’m ready for this."</button></a>
-                <a href="assets/brain.jpg"><button type="button" className="answer2">"I’ll just take what my advisor tells me to."</button></a>
-                <a href="assets/lungs.jpg"><button type="button" className="answer3">"I have no idea what I’m gonna do."</button></a>
-                <a href="assets/stomach.jpg"><button type="button" className="answer4">"Course selection is happening when?"</button></a>
+                {/* Replace below w link to results page */}
+                <a href={process.env.PUBLIC_URL + "/assets/heart.jpg"}><button type="button" className="answer1">"It’s alright- I’m ready for this."</button></a>
+                <a href={process.env.PUBLIC_URL + "/assets/brain.jpg"}><button type="button" className="answer2">"I’ll just take what my advisor tells me to."</button></a>
+                <a href={process.env.PUBLIC_URL + "/assets/lungs.jpg"}><button type="button" className="answer3">"I have no idea what I’m gonna do."</button></a>
+                <a href={process.env.PUBLIC_URL + "/assets/stomach.jpg"}><button type="button" className="answer4">"Course selection is happening when?"</button></a>
             </div>
         </div>
     )
